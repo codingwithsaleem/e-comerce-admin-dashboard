eafb48d917be69c4c8b51b6e961b53345662d302
saleem raza <sherohero33@gmail.com>
2025-05-26 11:16:28 +0500

    update

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+// import { ChevronLeft, ChevronRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
 
 import { cn } from "@/lib/utils";
@@ -52,8 +52,9 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
-        IconRight: () => <ChevronRight className="h-4 w-4" />,
+        // Removed invalid IconRight property
+        // IconLeft: () => <ChevronLeft className="size-4" />,
+        // IconRight: () => <ChevronRight className="size-4" />,
       }}
       {...props}
     />
