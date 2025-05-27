b407fe8f4a50cd54bdce2bbd57800e50f950b0fa
saleem raza <sherohero33@gmail.com>
2025-05-27 23:22:47 +0500

    image issue fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false,
+  images:{
+    domains: ['https://images.unsplash.com', 'images.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "**"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
